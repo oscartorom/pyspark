374cafd128aef5bd6c4618bc10232707b06e374d
Oscar Toro <oscartoromalaga@outlook.com>
2024-01-28 21:11:04 +1100

    dev: installed all packages to initiate Spark session

--- pyspark.py
+++ pyspark.py
@@ -1,7 +1,22 @@
 # Apache Spark practice
 import pyspark
-
-# Create dataset
 import os
+import pandas as pd
+# Create dataset
+
+
 def create_local_data():
-    os    
+    if 'data' not in os.listdir():
+        print('Creating local directory to store data')
+        os.mkdir('data')
+    
+    if 'initial_data.csv' not in os.listdir('data'):
+        data = pd.DataFrame(
+            {'Name': 'Oscsar', 'Age': 25, 'City':'Sydney'},
+            {'Name': 'Sofia', 'Age': 3, 'City': 'Sydney'},
+            {'Name': 'Random', 'Age': 78, 'City': 'Dubai'}
+        )
+
+        data.to_csv('data/initial_data.csv')
+
+create_local_data()
